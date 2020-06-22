f7a7eba829c0fcc3c04d211d7a76e40a326fcc5e
root <root@localhost.localdomain>
2020-06-22 22:14:09 +0800

    initial commit

---
+++ pkg/apis/willesxm/registry.go
@@ -0,0 +1,9 @@
+package willesxm
+
+const (
+	// GroupName k8s group name
+	GroupName = "willesxm.k8s.io"
+
+	// Version the current version
+	Version = "v1"
+)
---
+++ pkg/apis/willesxm/v1/doc.go
@@ -0,0 +1,5 @@
+// +k8s:deepcopy-gen=package
+
+// +groupName=willesxm.k8s.io
+
+package v1
